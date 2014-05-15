9902ff17039c0c93c3c34082342d8a7a0514c3d1
evansmith <smith.evanj@gmail.com>
2014-05-15 09:50:42 -0400

    test

--- app.rb
+++ app.rb
@@ -6,8 +6,7 @@ get '/' do
 	line_num = 0
 	@cake = []
 	File.open("cake.list").readlines.each do |line|
-		 @cake.push("#{line_num +=1}. #{line}")
-		 
+		 @cake.push("#{line_num +=1}. #{line}")	 
 	end
 	erb :index
 end
