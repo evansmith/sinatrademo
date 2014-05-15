88dd8ba442f1b1c59bc243266827342ac023345e
evansmith <smith.evanj@gmail.com>
2014-05-15 09:47:19 -0400

    Working list

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'erb'
 
 get '/' do
 	line_num = 0
+	@cake = []
 	File.open("cake.list").readlines.each do |line|
-		puts "#{line_num +=1} #{line}"
+		 @cake.push("#{line_num +=1}. #{line}")
+		 
 	end
+	erb :index
 end
+
