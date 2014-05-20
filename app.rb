a368c88f6ac47a524f87c6135af6fff6de791445
evansmith <smith.evanj@gmail.com>
2014-05-20 15:35:15 -0400

    using JSON

--- app.rb
+++ app.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/json'
+require 'json'
 require 'erb'
+
 
 get '/' do
 	line_num = 0
-	@cake = []
+	#cake_string = "<ol>"
+	@cakes = []
 	File.open("cake.list").readlines.each do |line|
-		 @cake.push("#{line_num +=1}. #{line}")	 
+		 #cake_string = cake_string+"<li>   #{line}  </li>"
+		 #{}"#{line_num +=1}. #{line}" 
+		 @cakes.push("#{line}")
+
 	end
-	erb :index
+	#@cakes.to_json
+	json(@cakes, :encoder => :to_json, :content_type => :js)
+	puts @cakes
+	
+	#cake_string = cake_string + "</ol>"
+	erb :index 
+	#return @cakes
+	#return cake_string
 end
 
