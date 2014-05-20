8235a8afa04f650bc610106bfa635943542d6ed1
evansmith <smith.evanj@gmail.com>
2014-05-20 16:14:00 -0400

    JSON parse working

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'erb'
 
 
 get '/' do
-	line_num = 0
+	#line_num = 0
 	#cake_string = "<ol>"
 	@cakes = []
 	File.open("cake.list").readlines.each do |line|
@@ -16,7 +16,7 @@ get '/' do
 
 	end
 	#@cakes.to_json
-	json(@cakes, :encoder => :to_json, :content_type => :js)
+	#json(@cakes, :encoder => :to_json, :content_type => :js)
 	puts @cakes
 	
 	#cake_string = cake_string + "</ol>"
@@ -24,4 +24,6 @@ get '/' do
 	#return @cakes
 	#return cake_string
 end
+
+
 
